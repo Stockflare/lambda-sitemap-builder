3276087b1bb1b926e3ff44d046078bf63f77a0ef
Mark Stratmann <mark@stratmann.me.uk>
2016-04-07 12:30:16 +0100

    Complete and deployed

--- README.md
+++ README.md
@@ -5,13 +5,24 @@ A lambda function that will be triggered from a scheduled CloudWatch event that 
 The CloudWatch Event must be configured to send the following JSON payload
 
 ```
-  'search_url': "https://api.stockflare.com/search/filter",
-  'search_method': "PUT",
-  'search_body': {
-    "conditions": { "sic": ">0" }
-  }
+{
+  "search_url": "https://api.stockflare.com/search/filter",
+  "site_url": "https://stockflare.com",
+  "search_body": {
+    "conditions": { "sic": ">0" },
+    "page": "1"
+  },
   "sitemap_bucket": "alpha.stockflare.com",
-  "sitemap": "sitemap.xml"
-
-
+  "max_pages": 30 <Used to limit the dataset in testing>
+}
 ```
+
+# What it does
+
+Firstly the function will use `when.iterate` to call `api-search` executing the filter query provided in the event.  The `iterate` will continue to fetch pages from `api-search` until all pages have been fetched.  The promise loop execute 10 asynchronous fetches to speed up processing and ensure that all pages are fetched within the lambda 5 minute timeout.  This process builds an array of `rics`.
+
+Next its again uses `when.iterate` to loop through the rics in chunks for 2000 elements and for each chunk will create a XML sitemap and upload it to S3.
+
+Next it will create a `sitmap.xml.gz` index file referencing each of the chunks and upload that to S3.
+
+And finally it pings Google telling them that the sitemap has been updated.
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // dependencies
-var aws = require('aws-sdk');
+var AWS = require('aws-sdk');
 var _ = require('underscore');
+var _l = require('lodash');
 var path = require('path');
 var when = require('when');
 var moment = require('moment');
 var rest = require('restler');
+var sm = require('sitemap');
+var fs = require('fs');
+var zlib = require('zlib');
+var str = require('string-to-stream');
+var builder = require('xmlbuilder');
 
 exports.handler = function(event, context) {
 
@@ -12,51 +18,219 @@ exports.handler = function(event, context) {
   console.log(event);
 
   var all_done = when.promise(function(all_done_resolve, all_done_reject, all_done_notify) {
-    
+
+    var total_pages = 9999;
+    var rics = [];
+    var max_pages = 99999;
+
+    if (!_.isUndefined(event.max_pages)) {
+      max_pages = event.max_pages;
+    }
+
+    when.iterate(function(next_page){
+      return next_page + 10;
+    },
+    function(page){
+      return page >= total_pages || page > max_pages;
+    }, function(page) {
+      return when.promise(function(resolve, reject, notify){
+
+        var get_rics = function(page) {
+          return when.promise(function(resolve, reject, notify){
+            var search_body = _.extend(event.search_body, {page: page, select: "ric"});
+            rest.put(event.search_url, {
+              data: JSON.stringify(search_body),
+            }).on('success', function(data, response){
+              if (data.length > 0) {
+                _.each(data, function(stock){
+                  rics.push(stock.ric);
+                });
+              }
+              console.log('Got page: ' + page);
+              console.log('Total Rics: ' + rics.length);
+              total_pages = parseInt(response.headers['x-api-pages']);
+              resolve();
+            }).on('fail', function(data, response){
+              console.log('Error:', data);
+              reject(data);
+            });
+          });
+        };
+
+        // Kick off 10 page fetches
+        var promises = _.map(_.range(page, page + 10), function(page){
+          return get_rics(page);
+        });
+
+        // When all fetches are done then let the loop continue
+        when.all(promises).done(function(){
+          resolve();
+        }, function(reason){
+          reject(reason);
+        });
+
+      });
+    },
+    1)
+    .done(function(){
+
+      var upload = function(content, filename) {
+        return when.promise(function(resolve, reject, notify){
+          // Upload sitemap to S3
+          s3Stream = require('s3-upload-stream')(new AWS.S3());
+          // Create the streams
+          var read = str(content);
+          var compress = zlib.createGzip();
+          var upload = s3Stream.upload({
+            "Bucket": event.sitemap_bucket,
+            "Key": filename
+          });
+
+          // Optional configuration
+          upload.maxPartSize(20971520); // 20 MB
+          upload.concurrentParts(5);
+
+          // Handle errors.
+          upload.on('error', function (error) {
+            console.log(error);
+            reject(error);
+          });
+
+          upload.on('part', function (details) {
+            console.log(details);
+          });
+
+          upload.on('uploaded', function (details) {
+            console.log(details);
+            resolve();
+          });
+
+          // Pipe the incoming stream through compression, and up to S3.
+          read.pipe(compress).pipe(upload);
+        });
+      };
+
+      var chunks = _l.chunk(rics, 2000);
+
+      when.iterate(function(index){
+        return index + 1;
+      },
+      function(index){
+        return index > (chunks.length - 1);
+      },
+      function(index){
+        return when.promise(function(resolve, reject, notify){
+          var chunk = chunks[index];
+          var urls = [
+            { url: '/' , changefreq: 'weekly', priority: 0.5 },
+            { url: '/landing' , changefreq: 'weekly', priority: 0.5 },
+          ];
+
+          var date = new Date();
+
+          _.each(chunk, function(ric){
+            urls.push({
+              url: '#!/stocks/' + ric , changefreq: 'daily', priority: 0.5
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/upside', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/growth', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/wallStreet', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/profitable', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/dividends', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/peers', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/description', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/stats', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/latest', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+            urls.push({
+              url: '#!/stocks/' + ric + '/breakdown/worth', changefreq: 'daily', priority: 0.5, lastmod: date
+            });
+          });
+
+          // Create the sitemap in memory
+          var sitemap = sm.createSitemap({
+              hostname: event.site_url,
+              cacheTime: 600000,  //600 sec (10 min) cache purge period
+              urls: urls
+          });
+
+          // Write the sitempa file
+          when(upload(sitemap.toString(), 'sitemap' + (index + 1) + '.xml.gz')).done(function(){
+            resolve();
+          }, function(reason){
+            reject(reason);
+          });
+        });
+      },
+      0).done(function(){
+        // Now create the Master sitemap index
+        var root = builder.create('sitemapindex', {encoding: 'UTF-8'}).att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+        // add in each sitemap
+        _.each(chunks, function(chunk, index){
+          var sitemap = root.ele('sitemap');
+          sitemap.ele('loc', event.site_url + '/sitemap' + (index + 1) + '.xml.gz');
+          sitemap.ele('lastmod', new Date().toISOString());
+        });
+
+        var xmlString = root.end({
+          pretty: true,
+          indent: '  ',
+          newline: '\n',
+          allowEmpty: false
+        });
+
+        // Uplad Master index sitemap
+        when(upload(xmlString, 'sitemap.xml.gz')).done(function(){
+          // now ping Google to tell them the sitemap is updated;
+          when.promise(function(resolve, reject, notify){
+            rest.get('http://google.com/ping?sitemap=' + event.site_url + '/sitemap.xml.gz')
+            .on('success', function(data, response){
+              console.log('Google Ping: ' + data);
+              resolve();
+            }).on('fail', function(data, response){
+              console.log('Google Ping Error:', data);
+              resolve();
+            });
+          }).done(function(){
+            all_done_resolve();
+          });
+
+        }, function(reason){
+          all_done_reject(reason);
+        });
+
+      }, function(reason){
+        all_done_reject(reason);
+      });
+
+    }, function(reason){
+      all_done_reject(reason);
+    });
+
   }).done(function(){
     console.log("Successfully created sitemap");
     context.succeed("Successfully created sitemap");
   }, function(reason){
     console.log("Failed to create sitemap: " + reason);
     context.fail("Failed to create sitemap: " + reason);
-  });
-
-
-
-  rest.get(event.fx_url).on('success', function(fx_result, response) {
-    console.log(fx_result);
-
-    // Do a post to api-currency fo every currency
-    var updated_at = Math.floor(new Date().getTime() / 1000);
-    promises = _.map(_.keys(fx_result), function(key, index, currencies){
-      return when.promise(function(resolve, reject, notify){
-        rest.post(event.currency_url, {
-          data: {
-            from: key,
-            to: 'usd',
-            rate: 1.0 / fx_result[key],
-            updated_at: updated_at
-          }
-        }).on('success', function(currency_result, response){
-          resolve(fx_result[key]);
-        }).on('fail', function(data, response){
-          console.log('Error:', data);
-          reject(data);
-        });
-      });
-    });
-
-    when.all(all_done).done(function(){
-      console.log("Successfully created sitemap");
-      context.succeed("Successfully created sitemap");
-    }, function(reason){
-      console.log("Failed to create sitemap: " + reason);
-      context.fail("Failed to create sitemap: " + reason);
-    });
-
-  }).on('fail', function(data, response){
-    console.log('Error:', data);
-    context.fail("Failed to process fx call");
   });
 
 };
